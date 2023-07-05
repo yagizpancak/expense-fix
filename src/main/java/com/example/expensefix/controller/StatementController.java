4fec27826a6aafde166650d2bbabfc6348255400
aybilgehan <bilgehanay@outlook.com>
2023-07-05 20:35:29 +0300

    kontrol edilmeden yapıldı. Cellerdeki bilgileri doğru şekilde çekip gelirleri pass etmesi gerekiyor. Tutulma şekline göre eklenmeye devam edilecek

--- src/main/java/com/example/expensefix/controller/StatementController.java
+++ src/main/java/com/example/expensefix/controller/StatementController.java
@@ -4,10 +4,7 @@ package com.example.expensefix.controller;
 import com.example.expensefix.service.StatementService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -19,9 +16,9 @@ public class StatementController {
 
     @PostMapping("/upload")
     public Boolean upload(
-            @Valid @RequestParam Integer userID,
-            @Valid @RequestParam MultipartFile file,
-            @Valid @RequestParam String bankName
+            @Valid @RequestBody Integer userID,
+            @Valid @RequestBody MultipartFile file,
+            @Valid @RequestBody String bankName
             )
     { return statementService.upload(userID, file, bankName); }
 
